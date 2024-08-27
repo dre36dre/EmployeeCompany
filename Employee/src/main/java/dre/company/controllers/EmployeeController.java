8cf8fa96d22e2d3505d277036ee800031ac367d1
dre36dre <dre.son36@gmail.com>
2024-08-27 11:58:10 -0300

    Endpoint Delete

--- Employee/src/main/java/dre/company/controllers/EmployeeController.java
+++ Employee/src/main/java/dre/company/controllers/EmployeeController.java
@@ -2,13 +2,23 @@ package dre.company.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import dre.company.entities.Employee;
 import dre.company.repositories.EmployeeRepository;
@@ -18,6 +28,8 @@ import dre.company.repositories.EmployeeRepository;
 public class EmployeeController {
 	
 	
+	private static final int String = 0;
+	private static final int ResponseEntity = 0;
 	@Autowired
 	private EmployeeRepository repository;
 	
@@ -35,6 +47,21 @@ public class EmployeeController {
 	public Employee  insert(@RequestBody  Employee employee){
 		Employee result=repository.save(employee);
 		return result;
+	}
+
+
+	@PutMapping("/{id}")
+	public Employee upDate(@PathVariable Long id, @RequestBody Employee employee) {
+		Employee current=repository.findById(id).get();
+		BeanUtils.copyProperties(employee, current, "id");
+		return repository.save(current);
+		
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 	
 	
--- Employee/src/main/java/dre/company/entities/Employee.java
+++ Employee/src/main/java/dre/company/entities/Employee.java
@@ -1,5 +1,6 @@
 package dre.company.entities;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
